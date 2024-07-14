cd28131fdd47b3490bf0ee5950bcd227cf18fa85
Yusuf Turlu <isyusufturlu@gmail.com>
2024-07-14 11:23:00 +0300

    Proje dosyası ekle.

---
+++ calculatorCs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
+
+                Console.WriteLine("Calculator Machine");
+
+                Console.Write("Please Enter First Number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please Enter Second Number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Here's the options: ");
+                Console.WriteLine("\t+ : Plus");
+                Console.WriteLine("\t- : Minus");
+                Console.WriteLine("\t* : Multiply");
+                Console.WriteLine("\t/ : Divide");
+                Console.Write("Enter what you want to with numbers ");
+
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid option");
+                        break;
+                }
+                Console.Write("Would you like to continue? (Y = yes, N = No): ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Program is Over.");
+            Console.ReadKey();
+        }
+    }
+}
